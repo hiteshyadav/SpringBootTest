40a6be59b2fc740ebf2deba0672056374d251e93
Hitesh <hmyadav87@yahoo.com>
2025-04-13 10:57:02 +0200

    Comment updated in web layer testing

--- SpringBootTest/src/test/java/com/example/controller/WebLayerTest.java
+++ SpringBootTest/src/test/java/com/example/controller/WebLayerTest.java
@@ -10,12 +10,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.example.repo.UserDto;
+
+/*
+ * In this case, only web layer will be loaded.
+ * Rest controller can be auto wired but dto object can not be auto wired.
+ */
 @WebMvcTest(FirstController.class)
 public class WebLayerTest {
 
 	@Autowired
 	private MockMvc mockMvc;
 
+//	@Autowired
+//	UserDto dto;
+	
+	@Autowired
+	FirstController controller;
+	
 	@Test
 	public void testController() throws Exception {
 		this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
